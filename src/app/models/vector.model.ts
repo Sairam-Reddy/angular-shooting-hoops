fa4298e2aa1cc3e0164f3567a1731c90777cd2b1
Sairam-Reddy <sairamreddyg33@gmail.com>
2022-01-20 23:26:22 +0530

    2 files modified, 2 added, 1 deleted

---
+++ src/app/models/vector.model.ts
@@ -0,0 +1,60 @@
+// A Vector
+export class Vector {
+  public _x: number = 1;
+  public _y: number = 0;
+
+  public static create(x: number, y: number): Vector {
+    const obj: Vector = new Vector();
+    obj.setX(x);
+    obj.setY(y);
+    return obj;
+  }
+
+  public setX(value: number): void {
+    this._x = value;
+  }
+
+  public getX(): number {
+    return this._x;
+  }
+
+  public setY(value: number): void {
+    this._y = value;
+  }
+
+  public getY(): number {
+    return this._y;
+  }
+
+  public setAngle(angle: number) {
+    const length = this.getLength();
+    this._x = Math.cos(angle) * length;
+    this._y = Math.sin(angle) * length;
+  }
+
+  public getAngle(): number {
+    return Math.atan2(this._y, this._x);
+  }
+
+  public setLength(length: number) {
+    const angle = this.getAngle();
+    this._x = Math.cos(angle) * length;
+    this._y = Math.sin(angle) * length;
+  }
+
+  public getLength(): number {
+    return Math.sqrt(this._x * this._x + this._y * this._y);
+  }
+
+  public add(v2: Vector): Vector {
+    return Vector.create(this._x + v2.getX(), this._y + v2.getY());
+  }
+
+  public subtract(v2: Vector): Vector {
+    return Vector.create(this._x - v2.getX(), this._y - v2.getY());
+  }
+
+  public scale(value: number): Vector {
+    return Vector.create(this._x * value, this._x * value);
+  }
+}
