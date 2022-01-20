ec0c91697c25fefd08b7c7b101c495d5dfb8790a
Sairam-Reddy <sairamreddyg33@gmail.com>
2022-01-20 23:30:27 +0530

    1 file added

---
+++ src/app/models/particle.model.ts
@@ -0,0 +1,28 @@
+import { Vector } from './vector.model';
+
+// A Particle
+export class Particle {
+  public position: Vector;
+  public velocity: Vector;
+  public gravity: Vector;
+
+  public static create(x, y, speed, direction, grav): Particle {
+    const obj = new Particle();
+    obj.position = Vector.create(x, y);
+    obj.velocity = Vector.create(0, 0);
+    obj.velocity.setLength(speed);
+    obj.velocity.setAngle(direction);
+    obj.gravity = Vector.create(0, grav || 0);
+
+    return obj;
+  }
+
+  public accelerate(vector: Vector) {
+    this.velocity = this.velocity.add(vector);
+  }
+
+  public update(): void {
+    this.velocity = this.velocity.add(this.gravity);
+    this.position = this.position.add(this.velocity);
+  }
+}
